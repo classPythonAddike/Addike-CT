8f743b9a1adb89601172d26ed8b1980c92e7f13c
Sanchit-Jain07 <sanchitjain1107@gmail.com>
2021-05-23 22:10:09 +0530

    Added Solution Component

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,16 @@
 <script>
-	export let name;
+    import Solution from './Solution.svelte'
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div class="container">
+    {#each Array(10) as i}
+        <Solution/>
+    {/each}
+</div>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+.container {
+    display: flex;
+    flex-wrap: wrap;
+}
 </style>
