429bd4431b3c718a1e9774b6104230e1f476d849
class PythonAddict <classpythonaddike@gmail.com>
2021-05-24 16:31:31 +0530

    Finished styling

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,109 @@
 <script>
-    import Solution from './Solution.svelte'
+  let ChallengeName = "Simon Says";
+  let ChallengeDescription =
+    "Only what Simon said counts! On each test, you will be given a  string of any length. This string can be anything. Your job is to output what Simon said!";
+
+  class Result {
+    constructor(file, time, length, progress) {
+      this.sol_name = file;
+      this.time_taken = time;
+      this.code_length = length;
+      this.progress = progress;
+    }
+  }
+
+  let result_array = [
+    RandomResult(),
+    RandomResult(),
+    RandomResult(),
+    RandomResult(),
+    RandomResult(),
+  ];
+
+  import Solution from "./Solution.svelte";
+
+  function RandomResult() {
+    return new Result("123456789.py", 45.67, 183, 56.4);
+  }
 </script>
 
+<table class="heading">
+  <tr>
+    <td class="title">
+      <h1>{ChallengeName}</h1>
+      <h4>Weekly Challenge</h4>
+    </td>
+    <td class="desc"><h5>{ChallengeDescription}</h5></td>
+  </tr>
+</table>
+
 <div class="container">
-    {#each Array(10) as i}
-        <Solution/>
-    {/each}
+  {#each result_array as { sol_name, time_taken, code_length, progress }, _}
+    <Solution {sol_name} {time_taken} {code_length} {progress} />
+  {/each}
 </div>
 
 <style>
-.container {
+  table {
+    margin: 20px;
+  }
+
+  .container {
     display: flex;
     flex-wrap: wrap;
-}
+  }
+
+  .heading {
+    color: black;
+    font-family: Roboto;
+  }
+
+  .title {
+    margin-left: 20px;
+    border-right: 1px black solid;
+    width: max-content;
+  }
+
+  td :last-child {
+    margin-left: 0px;
+  }
+
+  .desc {
+    padding-left: 10px;
+  }
+
+  h1 {
+    width: max-content;
+    margin-right: 10px;
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 45px;
+    line-height: 60px;
+    color: #000000;
+  }
+
+  h4 {
+    width: max-content;
+    text-align: left;
+    margin-left: 0;
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 27.2px;
+    line-height: 37px;
+    padding-right: 10px;
+  }
+
+  h5 {
+    width: 100%;
+    margin-left: 20px;
+    font-family: Montserrat;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 21.2px;
+    line-height: 30px;
+
+    color: #000000;
+  }
 </style>
--- src/Solution.svelte
+++ src/Solution.svelte
@@ -1,75 +1,155 @@
 <script>
-    import CircularProgress from '@smui/circular-progress';
-    export let sol_name = '123456789.py'
-    export let time_taken = 5.6
-    export let code_length = 55
-    export let progress = 50
+  //import CircularProgress from '@smui/circular-progress';
+  export let sol_name;
+  export let time_taken;
+  export let code_length;
+  export let progress;
 </script>
-  
+
 <div class="card">
-    <div class="name">
-        Solution: {sol_name}
-    </div>
-    <div class="progress">
-        {progress}%
-    </div>
-    <div class="info">
-        <div class="time"> 
-            Time Taken
-            <br/>
-            {time_taken}s
-        </div>
-        <div class="code">
-            Code Length
-            <br/>
-            {code_length} chars
-        </div>
-    </div>
+  <div class="name">
+    <img src="personicon.svg" class="personicon" />
+    <span>{sol_name}</span>
+  </div>
+  <div class="progress">
+    {progress}%
+  </div>
+  <div class="info">
+    <table class="information">
+      <tr>
+        <th class="first-item">Time Taken</th>
+        <th class="last-item">Code Length</th>
+      </tr>
+      <tr>
+        <td class="info-column">
+          <div class="time-placeholder">
+            <p class="time-data" style="display: inline;">{time_taken}</p>
+            <span class="time-unit">s</span>
+          </div>
+        </td>
+        <td class="info-column-2">
+          <div class="char-placeholder">
+            <p class="char-data" style="display: inline;">{code_length}</p>
+            <span class="char-unit">chars</span>
+          </div>
+        </td>
+      </tr>
+    </table>
+  </div>
 </div>
 
 <style>
-    .card {
-        height: 300px;
-        width: 300px;
-        background: #fff;
-        display: flex;
-        justify-content: space-around;
-        align-items: center;
-        flex-direction: column;
-        border-radius: 10px;
-        font-family: 'Montserrat', sans-serif;
-        margin: 20px;
-    }
-    .progress {
-        width: 100%;
-        height: 40%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-    .info {
-        height: 20%;
-        width: 100%;
-        display: flex;
-        justify-content: space-around;
-        align-items: center;
-    }
-    .code {
-        text-align: center;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        border-left: 1px solid black;
-    }
-    .time {
-        text-align: center;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        border-right: 1px solid black;
-    }
-    .code, .time {
-        width: 50%;
-        height: 100%;
-    }
+  .name {
+    display: flex;
+    justify-content: center;
+  }
+
+  .personicon {
+    height: 20px;
+    margin-left: 5px;
+  }
+
+  span {
+    padding-left: 5px;
+  }
+
+  .info-column-2 {
+    padding-left: 3px;
+    border: 0px;
+  }
+
+  table {
+    border-collapse: collapse;
+  }
+
+  table td,
+  table th {
+    border: 0px solid black;
+    border-right: 1px solid black;
+  }
+  table tr:first-child th {
+    border-top: 0;
+  }
+  table tr:last-child td {
+    border-bottom: 0;
+  }
+  table tr td:first-child,
+  table tr th:first-child {
+    border-right: 1px black solid;
+  }
+
+  .first-item {
+    padding-right: 5px;
+  }
+
+  .last-item {
+    border-right: 0px;
+    padding-left: 5px;
+  }
+
+  .time-placeholder,
+  .char-placeholder {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    font-family: Montserrat;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 13.3px;
+    line-height: 22px;
+  }
+
+  .card {
+    height: 340px;
+    width: 250px;
+    background: rgba(220, 220, 220, 0.7);
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    flex-direction: column;
+    border-radius: 13px;
+    font-family: "Montserrat", sans-serif;
+    margin: 9px;
+    box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.4);
+  }
+
+  .progress {
+    width: 100%;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 40.8px;
+    line-height: 56px;
+    text-align: center;
+    color: #000000;
+  }
+
+  .info {
+    height: 20%;
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+
+  .time-data,
+  .char-data {
+    font-family: Montserrat;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 15.3px;
+    line-height: 21px;
+    text-align: center;
+  }
+
+  .time-unit,
+  .char-unit {
+    color: #818181;
+    font-family: Montserrat;
+    font-weight: 400;
+  }
 </style>
